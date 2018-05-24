6695fd89a040bfe50965a3cc8a5288c456c7eb74
Sebastian Thiel <sthiel@thoughtworks.com>
2018-05-24 17:39:30 +0200

    Add first answer

--- src/lib.rs
+++ src/lib.rs
@@ -5,22 +5,27 @@ extern crate lazy_static;
 extern crate regex;
 
 use failure::{Error, ResultExt};
-use std::{fmt, io::{BufRead, BufReader, Read, Write}, str::FromStr};
+use std::{io::{BufRead, BufReader, Read, Write}, str::FromStr};
 
 lazy_static! {
-    static ref RE_SET_ROMAN: regex::Regex =
-        regex::Regex::new(r"^\s*(?P<value>\w+)\s+is\s+(?P<roman>\w)\s*$").expect("valid regex");
+    static ref RE_SET_ROMAN: regex::Regex = regex::Regex::new(
+        r"(?i)^\s*(?P<symbol>\w+)\s+is\s+(?P<roman>\w)\s*$"
+    ).expect("valid regex");
     static ref RE_ASSIGN_CREDITS: regex::Regex = regex::Regex::new(
-        r"^\s*(?P<values>[\w\s]+)\s+(?P<product>\w+)\s+is\s+(?P<credits>\d+)\s+[cC]redits$"
+        r"(?i)^\s*(?P<symbols>[\w\s]+)\s+(?P<product>\w+)\s+is\s+(?P<credits>\d+)\s+credits$"
+    ).expect("valid regex");
+    // how much is pish tegj glob glob ?
+    static ref RE_QUERY_ROMAN: regex::Regex = regex::Regex::new(
+        r"(?i)^\s*how\s*much\s+is\s+(?P<symbols>[\w\s]+)\s+\?\s*$"
     ).expect("valid regex");
 }
 
-fn roman_value(romans: &[Roman]) -> Result<u32, Error> {
+fn roman_to_decimal(romans: &[Roman]) -> Result<u32, Error> {
     Ok(match romans.len() {
-        0 => bail!("Cannot compute value for missing roman value"),
+        0 => bail!("Cannot compute decimal for missing roman value"),
         1 => romans[0].into(),
         _ => {
-            let mut value = 0;
+            let mut decimal = 0;
             let mut iter = romans.iter().peekable();
             while let Some(&c) = iter.next() {
                 let c: u32 = c.into();
@@ -31,31 +36,32 @@ fn roman_value(romans: &[Roman]) -> Result<u32, Error> {
                         multiplier = -1;
                     }
                 }
-                value += c as i64 * multiplier;
+                decimal += c as i64 * multiplier;
             }
-            if value < 0 {
-                bail!("Converted '{:?}' into negative value {}", romans, value);
+            if decimal < 0 {
+                bail!(
+                    "Converted '{:?}' into negative decimal value {}",
+                    romans,
+                    decimal
+                );
             }
-            value as u32
+            decimal as u32
         }
     })
 }
 
 #[derive(Debug, Default)]
 struct ConversionTable {
-    value_to_romans: Vec<(String, Roman)>,
+    symbol_to_romans: Vec<(String, Roman)>,
     product_prices: Vec<(String, u32)>,
 }
 
 impl ConversionTable {
-    fn translate_values_to_romans(
-        &self,
-        values_space_separated: &str,
-    ) -> Result<Vec<Roman>, Error> {
+    fn symbols_to_romans(&self, values_space_separated: &str) -> Result<Vec<Roman>, Error> {
         values_space_separated
             .split_whitespace()
             .map(|t| {
-                self.value_to_romans
+                self.symbol_to_romans
                     .iter()
                     .find(|(v, _)| v == t)
                     .map(|(_, r)| r.to_owned())
@@ -64,32 +70,34 @@ impl ConversionTable {
             .collect()
     }
 
+    fn symbols_to_decimal(&self, symbol_space_separated: &str) -> Result<u32, Error> {
+        roman_to_decimal(&self.symbols_to_romans(symbol_space_separated)?)
+    }
 
     fn update(
         &mut self,
         tokens: impl Iterator<Item = Result<Token, Error>>,
     ) -> Result<Vec<Query>, Error> {
         use self::Token::*;
-        let queries = Vec::new();
+        let mut queries = Vec::new();
         for token in tokens {
             match token {
                 Ok(token) => match token {
-                    RomanNumeralMapping { value, roman } => {
-                        self.value_to_romans.push((value, roman));
+                    Other(query) => queries.push(query),
+                    RomanNumeralMapping { symbol, roman } => {
+                        self.symbol_to_romans.push((symbol, roman));
                     }
                     PriceAssignment {
                         credits,
                         product,
-                        values_space_separated,
+                        symbols_space_separated,
                     } => {
                         let product_price = credits
-                            .checked_div(roman_value(&self.translate_values_to_romans(
-                                &values_space_separated,
-                            )?)?)
+                            .checked_div(self.symbols_to_decimal(&symbols_space_separated)?)
                             .ok_or_else(|| {
                                 format_err!(
                                     "The roman value corresponding to '{}' was invalid.",
-                                    values_space_separated
+                                    symbols_space_separated
                                 )
                             })?;
                         self.product_prices.push((product, product_price));
@@ -103,7 +111,20 @@ impl ConversionTable {
 }
 
 enum Query {
+    Roman { symbols_space_separated: String },
+}
 
+impl Query {
+    fn answer(&self, table: &ConversionTable) -> Result<String, Error> {
+        Ok(match self {
+            Query::Roman {
+                symbols_space_separated,
+            } => {
+                let decimal_value = table.symbols_to_decimal(&symbols_space_separated)?;
+                format!("{} is {}", symbols_space_separated, decimal_value)
+            }
+        })
+    }
 }
 
 #[derive(Debug, Hash, Clone, Copy)]
@@ -152,47 +173,45 @@ impl From<Roman> for u32 {
 
 enum Token {
     RomanNumeralMapping {
-        value: String,
+        symbol: String,
         roman: Roman,
     },
     PriceAssignment {
         credits: u32,
         product: String,
-        values_space_separated: String,
+        symbols_space_separated: String,
     },
-}
-
-struct Answer<'a>(&'a Query, &'a ConversionTable);
-
-impl<'a> fmt::Display for Answer<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        unimplemented!()
-    }
+    Other(Query),
 }
 
 impl FromStr for Token {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Token, Error> {
-        Ok(
-            match (RE_SET_ROMAN.captures(s), RE_ASSIGN_CREDITS.captures(s)) {
-                (Some(captures), None) => Token::RomanNumeralMapping {
-                    value: captures["value"].to_owned(),
-                    roman: captures["roman"].parse()?,
-                },
-                (None, Some(captures)) => Token::PriceAssignment {
-                    credits: captures["credits"].parse::<u32>().with_context(|_| {
-                        format!(
-                            "Could not obtain unsigned integer from '{}'",
-                            &captures["credits"]
-                        )
-                    })?,
-                    product: captures["product"].to_owned(),
-                    values_space_separated: captures["values"].to_owned(),
-                },
-                _ => return Err(format_err!("'{}' could not be parsed", s)),
-            },
-        )
+        use self::Token::*;
+        Ok(if let Some(captures) = RE_SET_ROMAN.captures(s) {
+            RomanNumeralMapping {
+                symbol: captures["symbol"].to_owned(),
+                roman: captures["roman"].parse()?,
+            }
+        } else if let Some(captures) = RE_ASSIGN_CREDITS.captures(s) {
+            PriceAssignment {
+                credits: captures["credits"].parse::<u32>().with_context(|_| {
+                    format!(
+                        "Could not obtain unsigned integer from '{}'",
+                        &captures["credits"]
+                    )
+                })?,
+                product: captures["product"].to_owned(),
+                symbols_space_separated: captures["symbols"].to_owned(),
+            }
+        } else if let Some(captures) = RE_QUERY_ROMAN.captures(s) {
+            Other(Query::Roman {
+                symbols_space_separated: captures["symbols"].to_owned(),
+            })
+        } else {
+            return Err(format_err!("'{}' could not be parsed", s));
+        })
     }
 }
 
@@ -209,10 +228,10 @@ pub fn answers(input: impl Read, mut output: impl Write) -> Result<(), Error> {
     let mut table = ConversionTable::default();
     let queries = table.update(parse(input))?;
     if queries.is_empty() {
-        writeln!(output, "{:?}", table)?;
+        writeln!(output, "{:#?}", table)?;
     } else {
         for query in queries {
-            writeln!(output, "{}", Answer(&query, &table))?;
+            writeln!(output, "{}", query.answer(&table)?)?;
         }
     }
     Ok(())

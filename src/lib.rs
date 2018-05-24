9258a7813bc108bfdc2a3ad796c3cdc664ff0f57
Sebastian Thiel <sthiel@thoughtworks.com>
2018-05-24 09:38:50 +0200

    A step closer towards computing prices
    Next up is converting Roman values.
    This would be an exellent case for unit-tests actually

--- src/lib.rs
+++ src/lib.rs
@@ -5,25 +5,72 @@ extern crate lazy_static;
 extern crate regex;
 
 use failure::{Error, ResultExt};
-use std::{fmt, io::{BufRead, BufReader, Read, Write}, str::FromStr};
+use std::{fmt, collections::HashMap, io::{BufRead, BufReader, Read, Write}, str::FromStr};
 
 lazy_static! {
     static ref RE_SET_ROMAN: regex::Regex =
         regex::Regex::new(r"^\s*(?P<value>\w+)\s+is\s+(?P<roman>\w)\s*$").expect("valid regex");
+//        glob glob Silver is 34 Credits
+    static ref RE_ASSIGN_CREDITS: regex::Regex =
+        regex::Regex::new(r"^\s*(?P<values>\w+\s+)+(?P<product>\w+)\s+is\s+(?P<credits>\d+)\s+[cC]redits$").expect("valid regex");
 }
 
 #[derive(Debug, Default)]
-struct ConversionTable {}
+struct ConversionTable {
+    value_to_romans: HashMap<String, Roman>,
+    product_prices: HashMap<String, u32>,
+}
 
 impl ConversionTable {
+    fn translate_values_to_romans(
+        &self,
+        values_space_separated: &str,
+    ) -> Result<Vec<Roman>, Error> {
+        values_space_separated
+            .split_whitespace()
+            .map(|t| {
+                self.value_to_romans
+                    .get(t)
+                    .map(ToOwned::to_owned)
+                    .ok_or_else(|| format_err!("No roman value was associated with '{}'", t))
+            })
+            .collect()
+    }
+
+    fn roman_value(&self, romans: &[Roman]) -> Result<u32, Error> {
+        unimplemented!()
+    }
+
     fn update(
         &mut self,
         tokens: impl Iterator<Item = Result<Token, Error>>,
     ) -> Result<Vec<Query>, Error> {
-        let mut queries = Vec::new();
+        use self::Token::*;
+        let queries = Vec::new();
         for token in tokens {
             match token {
-                Ok(token) => unimplemented!(),
+                Ok(token) => match token {
+                    RomanNumeralMapping { value, roman } => {
+                        self.value_to_romans.insert(value, roman);
+                    }
+                    PriceAssignment {
+                        credits,
+                        product,
+                        values_space_separated,
+                    } => {
+                        let product_price = credits
+                            .checked_div(self.roman_value(&self.translate_values_to_romans(
+                                &values_space_separated,
+                            )?)?)
+                            .ok_or_else(|| {
+                                format_err!(
+                                    "The roman value corresponding to '{}' was invalid.",
+                                    values_space_separated
+                                )
+                            })?;
+                        self.product_prices.insert(product, product_price);
+                    }
+                },
                 Err(err) => return Err(err),
             }
         }
@@ -35,6 +82,7 @@ enum Query {
 
 }
 
+#[derive(Debug, Hash, Clone)]
 enum Roman {
     I,
     V,
@@ -79,7 +127,15 @@ impl From<Roman> for u32 {
 }
 
 enum Token {
-    RomanNumeralMapping { value: String, roman: Roman },
+    RomanNumeralMapping {
+        value: String,
+        roman: Roman,
+    },
+    PriceAssignment {
+        credits: u32,
+        product: String,
+        values_space_separated: String,
+    },
 }
 
 struct Answer<'a>(&'a Query, &'a ConversionTable);
@@ -93,14 +149,26 @@ impl<'a> fmt::Display for Answer<'a> {
 impl FromStr for Token {
     type Err = Error;
 
-    fn from_str(s: &str) -> Result<Self, <Self as FromStr>::Err> {
-        match RE_SET_ROMAN.captures(s) {
-            Some(captures) => Ok(Token::RomanNumeralMapping {
-                value: captures["value"].to_owned(),
-                roman: captures["roman"].parse()?,
-            }),
-            _ => Err(format_err!("'{}' could not be parsed", s)),
-        }
+    fn from_str(s: &str) -> Result<Token, Error> {
+        Ok(
+            match (RE_SET_ROMAN.captures(s), RE_ASSIGN_CREDITS.captures(s)) {
+                (Some(captures), None) => Token::RomanNumeralMapping {
+                    value: captures["value"].to_owned(),
+                    roman: captures["roman"].parse()?,
+                },
+                (None, Some(captures)) => Token::PriceAssignment {
+                    credits: captures["credits"].parse::<u32>().with_context(|_| {
+                        format!(
+                            "Could not obtain unsigned integer from '{}'",
+                            &captures["credits"]
+                        )
+                    })?,
+                    product: captures["product"].to_owned(),
+                    values_space_separated: captures["values"].to_owned(),
+                },
+                _ => return Err(format_err!("'{}' could not be parsed", s)),
+            },
+        )
     }
 }
 

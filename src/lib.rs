e7a76fa779e0557a9e6aa83ac4b2aaa5ddd11f94
Sebastian Thiel <sthiel@thoughtworks.com>
2018-05-25 08:53:38 +0200

    Some minor refactoring - it's DONE

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,59 @@ extern crate failure;
 use failure::{Error, ResultExt};
 use std::{collections::BTreeMap, io::{BufRead, BufReader, Read, Write}, str::FromStr};
 
+pub fn answers(input: impl Read, mut output: impl Write) -> Result<(), Error> {
+    let mut symbol_to_romans: BTreeMap<String, Roman> = BTreeMap::new();
+    let mut product_prices: BTreeMap<String, f32> = BTreeMap::new();
+    let input = BufReader::new(input);
+
+    for line in input.lines() {
+        let line = line.context("Failed to read at least one line from input")?;
+        let tokens: Vec<_> = line.split_whitespace().collect();
+        match *tokens {
+            [symbol, "is", roman] => {
+                if !symbol_to_romans.contains_key(symbol) {
+                    symbol_to_romans.insert(symbol.to_owned(), roman.parse()?);
+                }
+            }
+            [ref symbols.., product, "is", credits, "Credits"] => {
+                let credits = credits.parse::<f32>().with_context(|_| {
+                    format!("Could not parse floating point number from '{}'", credits)
+                })?;
+                if !product_prices.contains_key(product) {
+                    let product_price =
+                        credits / symbols_to_decimal(&symbol_to_romans, symbols.iter())? as f32;
+                    product_prices.insert(product.to_owned(), product_price);
+                }
+            }
+            ["how", "much", "is", ref symbols.., "?"] => {
+                let decimal_value = symbols_to_decimal(&symbol_to_romans, symbols.iter())?;
+                writeln!(output, "{} is {}", symbols.join(" "), decimal_value)?;
+            }
+            ["how", "many", "Credits", "is", ref symbols.., product, "?"] => {
+                let single_product_price = product_prices.get(product).ok_or_else(|| {
+                    format_err!("Product named '{}' was not yet encountered", product)
+                })?;
+                let decimal_multiplier = symbols_to_decimal(&symbol_to_romans, symbols.iter())?;
+                let product_price = decimal_multiplier as f32 * single_product_price;
+                writeln!(
+                    output,
+                    "{} {} is {} Credits",
+                    symbols.join(" "),
+                    product,
+                    product_price
+                )?;
+            }
+            ["how", "much", _.., "?"] => {
+                writeln!(output, "I have no idea what you are talking about")?;
+            }
+            _ => {
+                return Err(format_err!("'{}' could not be parsed", line));
+            }
+        }
+    }
+    Ok(())
+}
+
 #[derive(Debug, Ord, PartialOrd, PartialEq, Eq, Clone, Copy)]
 enum Roman {
     I,
@@ -90,57 +143,4 @@ fn symbols_to_decimal(
     symbols: impl Iterator<Item = impl AsRef<str>>,
 ) -> Result<u32, Error> {
     roman_to_decimal(symbols_to_romans(symbol_table, symbols)?.into_iter())
-}
-
-pub fn answers(input: impl Read, mut output: impl Write) -> Result<(), Error> {
-    let mut symbol_to_romans: BTreeMap<String, Roman> = BTreeMap::new();
-    let mut product_prices: BTreeMap<String, f32> = BTreeMap::new();
-    let input = BufReader::new(input);
-
-    for line in input.lines() {
-        let line = line.context("Failed to read at least one line from input")?;
-        let tokens: Vec<_> = line.split_whitespace().collect();
-        match *tokens {
-            [symbol, "is", roman] => {
-                if !symbol_to_romans.contains_key(symbol) {
-                    symbol_to_romans.insert(symbol.to_owned(), roman.parse()?);
-                }
-            }
-            [ref symbols.., product, "is", credits, "Credits"] => {
-                let credits = credits.parse::<f32>().with_context(|_| {
-                    format!("Could not parse floating point number from '{}'", credits)
-                })?;
-                if !product_prices.contains_key(product) {
-                    let product_price =
-                        credits / symbols_to_decimal(&symbol_to_romans, symbols.iter())? as f32;
-                    product_prices.insert(product.to_owned(), product_price);
-                }
-            }
-            ["how", "much", "is", ref symbols.., "?"] => {
-                let decimal_value = symbols_to_decimal(&symbol_to_romans, symbols.iter())?;
-                writeln!(output, "{} is {}", symbols.join(" "), decimal_value)?;
-            }
-            ["how", "many", "Credits", "is", ref symbols.., product, "?"] => {
-                let single_product_price = product_prices.get(product).ok_or_else(|| {
-                    format_err!("Product named '{}' was not yet encountered", product)
-                })?;
-                let decimal_multiplier = symbols_to_decimal(&symbol_to_romans, symbols.iter())?;
-                let product_price = decimal_multiplier as f32 * single_product_price;
-                writeln!(
-                    output,
-                    "{} {} is {} Credits",
-                    symbols.join(" "),
-                    product,
-                    product_price
-                )?;
-            }
-            ["how", "much", _.., "?"] => {
-                writeln!(output, "I have no idea what you are talking about")?;
-            }
-            _ => {
-                return Err(format_err!("'{}' could not be parsed", line));
-            }
-        }
-    }
-    Ok(())
 }

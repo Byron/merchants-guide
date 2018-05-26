0eaba074bfe78891091cf36c233fe91c2acd999f
Sebastian Thiel <byronimo@gmail.com>
2018-05-26 12:05:08 +0200

    Get rid of some boilerplate
    The latter is used in many places and can now be found in a tiny crate

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,11 @@
 #[macro_use]
 extern crate failure;
+extern crate failure_tools;
 extern crate galactic_merchants_guide;
 
+use failure_tools::ok_or_exit;
 use failure::{Error, ResultExt};
-use std::{env, process, fs::File, io::{stderr, stdout, Write}};
-
-// TODO: put this into a crate, like 'failure-print' or 'failure-chain'
-pub fn print_causes<E, W>(e: E, mut w: W)
-where
-    E: Into<Error>,
-    W: Write,
-{
-    let e = e.into();
-    let causes = e.causes().collect::<Vec<_>>();
-    let num_causes = causes.len();
-    for (index, cause) in causes.iter().enumerate() {
-        if index == 0 {
-            writeln!(w, "{}", cause).ok();
-            if num_causes > 1 {
-                writeln!(w, "Caused by: ").ok();
-            }
-        } else {
-            writeln!(w, " {}: {}", num_causes - index, cause).ok();
-        }
-    }
-}
-
-pub fn ok_or_exit<T, E>(r: Result<T, E>) -> T
-where
-    E: Into<Error>,
-{
-    match r {
-        Ok(r) => r,
-        Err(e) => {
-            stdout().flush().ok();
-            write!(stderr(), "error: ").ok();
-            print_causes(e, stderr());
-            process::exit(1);
-        }
-    }
-}
+use std::{env, fs::File, io::stdout};
 
 fn run() -> Result<(), Error> {
     let filename = env::args().nth(1).ok_or_else(|| {
